fd6452e93c16d1db09cae76baa7af8169f97c9d4
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-09-17 08:46:28 -0300

    f

--- perguntas.js
+++ perguntas.js
@@ -1,99 +1,99 @@
 criaCartao(
-    'biologia',
-    'Quais são os principais tecidos presentes no corpo humano?',
-    'tecido epitelial, tecido conjuntivo, tecido muscular e tecido nervoso')
-    
-    criaCartao(
-    'biologia',
-    'Quais são as fases do desenvolvimento embrionário?',
-    'Zigoto, mórula, blastocisto e gástrula')
-    
-    criaCartao(
-    'biologia',
-    'Quais são as estruturas presentes no sistema nervoso?',
-    'Encéfalo, medula, nervos e gânglios')
-    
-    criaCartao(
-    'biologia',
-    'Qual a principal estrutura do encéfalo?',
-    'Cérebro')
-    
-    criaCartao(
-    'biologia',
-    'Qual o sistema responsável por movimentos involuntários do corpo?',
-    'Sistema nervoso autônomo')
-    
-    criaCartao(
-    'história',
-    'O que foi o iluminismo?',
-    'movimento criado na Europa entre os séculos XVII e XVIII que buscava valorizar o pensamento e a razão')
-    
-    criaCartao(
-    'história',
-    'Em que ano ocorreu a queda do muro de Berlim?',
-    'Em 09/10/1989')
-    
-    criaCartao(
-    'história',
-    'O que foi o feudalismo?',
-    'Foi um movimento ocorrido na Europa durante a idade média. Um sistema político e econômico baseado no plantio e uso do solo, formado por clero, nobreza e servo')
-    
-    criaCartao(
-    'história',
-    'Em que período ocorreu a revolução francesa?',
-    'Entre 05/05/1789 e 09/11/1799')
-    
-    criaCartao(
-    'história',
-    'Em que período ocorreu a idade média?',
-    'Se início no século V após a queda do Império Romano do Ocidente em 476 e durou até o século XV com a tomada de Constantinopla pelo Império Otomano em 1453')
-    
-    criaCartao(
-    'geografia',
-    'O que é a OTAN?',
-    'Aliança militar baseada em um tratado de segurança coletiva, com o objetivo de promover a paz entre seus membros')
-    
-    criaCartao(
-    'geografia',
-    'O que é o BRICS?',
-    'Grupo de países de mercado emergentes que buscam a cooperação socioeconômica para promover o crescimento desses países')
-    
-    criaCartao(
-    'geografia',
-    'Quais os três principais relevos do Brasil?',
-    'Planaltos, planícies e depressões')
-    
-    criaCartao(
-    'geografia',
-    'Quais os principais blocos econômicos do mundo?',
-    'União européia, Mercosul, Usmca, Asean, Cei, Efta e Sacu')
-    
-    criaCartao(
-    'sociologia',
-    'Quem criou o termo “indústria cultural”?',
-    'Theodor Adorno e Max Horkheimer')
-    
-    criaCartao(
-    'química',
-    'O que são reações endotérmicas?',
-    'São reações onde há resfriação e a temperatura dos produtos é menor do que a dos reagentes')
-    
-    criaCartao(
-    'química',
-    'O que são reações exotérmicas?',
-    'São reações onde há o aumento da temperatura e a temperatura dos produtos é maior que a dos reagentes')
-    
-    criaCartao(
-    'química',
-    'Como calcular o número atômico de um elemento?',
-    'O número atômico é igual ao número de prótons do núcleo')
-    
-    criaCartao(
-    'matemática',
-    'Qual a fórmula pra soma dos termos de uma ',
+    'biology',
+    'What are the main tissues present in the human body?',
+    'Epithelial tissue, connective tissue, muscular tissue, and nervous tissue')
+
+criaCartao(
+    'biology',
+    'What are the stages of embryonic development?',
+    'Zygote, morula, blastocyst, and gastrula')
+
+criaCartao(
+    'biology',
+    'What are the structures present in the nervous system?',
+    'Brain, spinal cord, nerves, and ganglia')
+
+criaCartao(
+    'biology',
+    'What is the main structure of the brain?',
+    'Brain')
+
+criaCartao(
+    'biology',
+    'Which system is responsible for involuntary body movements?',
+    'Autonomic nervous system')
+
+criaCartao(
+    'history',
+    'What was the Enlightenment?',
+    'A movement created in Europe between the 17th and 18th centuries that sought to value thought and reason')
+
+criaCartao(
+    'history',
+    'In what year did the Berlin Wall fall?',
+    'On 09/10/1989')
+
+criaCartao(
+    'history',
+    'What was feudalism?',
+    'A movement that occurred in Europe during the Middle Ages. A political and economic system based on land cultivation and use, formed by clergy, nobility, and serfs')
+
+criaCartao(
+    'history',
+    'During what period did the French Revolution occur?',
+    'Between 05/05/1789 and 09/11/1799')
+
+criaCartao(
+    'history',
+    'During what period did the Middle Ages occur?',
+    'It started in the 5th century after the fall of the Western Roman Empire in 476 and lasted until the 15th century with the capture of Constantinople by the Ottoman Empire in 1453')
+
+criaCartao(
+    'geography',
+    'What is NATO?',
+    'A military alliance based on a collective security treaty, aimed at promoting peace among its members')
+
+criaCartao(
+    'geography',
+    'What is BRICS?',
+    'A group of emerging market countries that seek socioeconomic cooperation to promote the growth of these countries')
+
+criaCartao(
+    'geography',
+    'What are the three main types of relief in Brazil?',
+    'Plateaus, plains, and depressions')
+
+criaCartao(
+    'geography',
+    'What are the main economic blocs in the world?',
+    'European Union, Mercosur, USMCA, ASEAN, CIS, EFTA, and SACU')
+
+criaCartao(
+    'sociology',
+    'Who coined the term “cultural industry”?',
+    'Theodor Adorno and Max Horkheimer')
+
+criaCartao(
+    'chemistry',
+    'What are endothermic reactions?',
+    'Reactions where there is absorption of heat and the temperature of the products is lower than that of the reactants')
+
+criaCartao(
+    'chemistry',
+    'What are exothermic reactions?',
+    'Reactions where there is a release of heat and the temperature of the products is higher than that of the reactants')
+
+criaCartao(
+    'chemistry',
+    'How do you calculate the atomic number of an element?',
+    'The atomic number is equal to the number of protons in the nucleus')
+
+criaCartao(
+    'mathematics',
+    'What is the formula for the sum of the terms of a geometric sequence?',
     'an + 1 = an . q')
-    
-    criaCartao(
-    'física',
-    'Qual a fórmula utilizada para calcular o movimento uniforme',
-    'S = S⁰ + V . T')
+
+criaCartao(
+    'physics',
+    'What is the formula used to calculate uniform motion?',
+    'S = S₀ + V . T')
