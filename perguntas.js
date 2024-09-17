e92c1c6b4bcedb9e18239d1727a49b138f3b04b3
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-09-17 08:30:10 -0300

    fg

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'biologia',
+    'Quais são os principais tecidos presentes no corpo humano?',
+    'tecido epitelial, tecido conjuntivo, tecido muscular e tecido nervoso')
+    
+    criaCartao(
+    'biologia',
+    'Quais são as fases do desenvolvimento embrionário?',
+    'Zigoto, mórula, blastocisto e gástrula')
+    
+    criaCartao(
+    'biologia',
+    'Quais são as estruturas presentes no sistema nervoso?',
+    'Encéfalo, medula, nervos e gânglios')
+    
+    criaCartao(
+    'biologia',
+    'Qual a principal estrutura do encéfalo?',
+    'Cérebro')
+    
+    criaCartao(
+    'biologia',
+    'Qual o sistema responsável por movimentos involuntários do corpo?',
+    'Sistema nervoso autônomo')
+    
+    criaCartao(
+    'história',
+    'O que foi o iluminismo?',
+    'movimento criado na Europa entre os séculos XVII e XVIII que buscava valorizar o pensamento e a razão')
+    
+    criaCartao(
+    'história',
+    'Em que ano ocorreu a queda do muro de Berlim?',
+    'Em 09/10/1989')
+    
+    criaCartao(
+    'história',
+    'O que foi o feudalismo?',
+    'Foi um movimento ocorrido na Europa durante a idade média. Um sistema político e econômico baseado no plantio e uso do solo, formado por clero, nobreza e servo')
+    
+    criaCartao(
+    'história',
+    'Em que período ocorreu a revolução francesa?',
+    'Entre 05/05/1789 e 09/11/1799')
+    
+    criaCartao(
+    'história',
+    'Em que período ocorreu a idade média?',
+    'Se início no século V após a queda do Império Romano do Ocidente em 476 e durou até o século XV com a tomada de Constantinopla pelo Império Otomano em 1453')
+    
+    criaCartao(
+    'geografia',
+    'O que é a OTAN?',
+    'Aliança militar baseada em um tratado de segurança coletiva, com o objetivo de promover a paz entre seus membros')
+    
+    criaCartao(
+    'geografia',
+    'O que é o BRICS?',
+    'Grupo de países de mercado emergentes que buscam a cooperação socioeconômica para promover o crescimento desses países')
+    
+    criaCartao(
+    'geografia',
+    'Quais os três principais relevos do Brasil?',
+    'Planaltos, planícies e depressões')
+    
+    criaCartao(
+    'geografia',
+    'Quais os principais blocos econômicos do mundo?',
+    'União européia, Mercosul, Usmca, Asean, Cei, Efta e Sacu')
+    
+    criaCartao(
+    'sociologia',
+    'Quem criou o termo “indústria cultural”?',
+    'Theodor Adorno e Max Horkheimer')
+    
+    criaCartao(
+    'química',
+    'O que são reações endotérmicas?',
+    'São reações onde há resfriação e a temperatura dos produtos é menor do que a dos reagentes')
+    
+    criaCartao(
+    'química',
+    'O que são reações exotérmicas?',
+    'São reações onde há o aumento da temperatura e a temperatura dos produtos é maior que a dos reagentes')
+    
+    criaCartao(
+    'química',
+    'Como calcular o número atômico de um elemento?',
+    'O número atômico é igual ao número de prótons do núcleo')
+    
+    criaCartao(
+    'matemática',
+    'Qual a fórmula pra soma dos termos de uma ',
+    'an + 1 = an . q')
+    
+    criaCartao(
+    'física',
+    'Qual a fórmula utilizada para calcular o movimento uniforme',
+    'S = S⁰ + V . T')
