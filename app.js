92111ef493cee706e409c726cea270f6eb0d3cb2
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-09-26 08:02:28 -0300

    c

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-
+function criaCartão(categoria, pergunta, resposta){
+    let container = document.getElementById('container0');
+    let cartao = document.createElement('article');
+    cartao.className='cartao';
+    cartao.innerHTML=`
+    <div class="cartao-conteudo">
+    <b3>${categoria}</h3>
+    <div class="carao-pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao-resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    container.appendChild(cartao);
+}
