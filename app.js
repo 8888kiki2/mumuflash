c9ff97ee4b850dc9c4c9209a11878e9395ff2142
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-10-03 07:58:49 -0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.className='cartao';
     cartao.innerHTML=`
     <div class="cartao-conteudo">
-    <b3>${categoria}</h3>
+    <h3>${categoria}</h3>
     <div class="carao-pergunta">
     <p>${pergunta}</p>
     </div>
